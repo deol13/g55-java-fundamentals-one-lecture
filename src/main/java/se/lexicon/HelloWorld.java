022ff9ed950c6ce09518a2abc09b424d8bb712b6
Dennis Olsen <dennan_lol@hotmail.com>
2025-03-04 10:05:18 +0100

    Variables

--- src/main/java/se/lexicon/HelloWorld.java
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -15,7 +15,28 @@ public class HelloWorld {
      */
     // psvm + Tab
     public static void main(String[] args) {
+
         // sout + tab
         System.out.println("Hello World!");
+
+        // Variables
+        // [data type] [variableName]
+        int speed;
+        speed = 100;
+
+        double salary = 50000.55;
+        char grade = 'A';
+        char specialChar = '\u2764'; // A heart character.
+        boolean isActive = true;
+        boolean isReserved = false;
+
+        long balance = 500000000000L; // You need to end it with l or L to show its a long, otherwise it will be taken as an int.
+        double area = 20.55;
+        int setNumber = 100;
+        int studentId = 5000;
+
+        System.out.println("Speed: " + speed);
     }
+
+    // Reformat the code: Ctrl + Alt + L
 }
