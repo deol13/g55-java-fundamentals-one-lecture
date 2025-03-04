a9b57585a19efaaa2a8cdf7e181a283e420b2b24
Dennis Olsen <dennan_lol@hotmail.com>
2025-03-04 09:37:51 +0100

    README

--- src/main/java/se/lexicon/HelloWorld.java
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -1,4 +1,21 @@
 package se.lexicon;
 
+
 public class HelloWorld {
+    //This is a single line comment
+
+    /*
+    This is a regular multi-line comment
+    */
+
+    /**
+     * This is a Java Doc (Documentation comment)
+     * ##Used to create documentation for **Java Code**
+     * @param args
+     */
+    // psvm + Tab
+    public static void main(String[] args) {
+        // sout + tab
+        System.out.println("Hello World!");
+    }
 }
