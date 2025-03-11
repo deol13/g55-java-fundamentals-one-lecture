f1832a6c46ecfee019ce09446ac49c998dc8d721
Dennis Olsen <dennan_lol@hotmail.com>
2025-03-11 11:31:27 +0100

    static and instant fields number generator demo

--- src/main/java/se/lexicon/part2/Main.java
+++ src/main/java/se/lexicon/part2/Main.java
@@ -2,6 +2,10 @@ package se.lexicon.part2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MathOperator.add(25.0, 20));
+        //System.out.println(MathOperator.add(25.0, 20));
+
+        System.out.println(NumberGenerator.generateRandomNumber());
+        System.out.println(NumberGenerator.generateRandomNumber(10, 100));
+        System.out.println(NumberGenerator.generateRandomUUID());
     }
 }
--- src/main/java/se/lexicon/part2/NumberGenerator.java
+++ src/main/java/se/lexicon/part2/NumberGenerator.java
@@ -1,4 +1,23 @@
 package se.lexicon.part2;
 
+import java.util.Random;
+import java.util.UUID;
+
 public class NumberGenerator {
+
+    public static int generateRandomNumber(){
+        Random random = new Random();
+        return random.nextInt();
+    }
+
+    public static int generateRandomNumber(int min, int max){ //min: 10 - max: 80
+        Random random = new Random();
+        // It generats a number between 0 and max, so we need to reduce max by min before so we can later add it back to make it between min and max.
+        // nextInt(0-79) so we need to reduce it to nextInt(0-69) then add 1 and min(10) to make it 10-80
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public static String generateRandomUUID(){
+        return UUID.randomUUID().toString();
+    }
 }
